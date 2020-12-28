05d5fd8a0ea8c5bc9e51a9eea0ac64066e353929
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2020-12-28 21:12:55 +0300

    a

--- Earley.cpp
+++ Earley.cpp
@@ -1,15 +1,21 @@
 #include "Earley.h"
 
-
-
 Earley::Earley(ContextFreeGrammar) {
-
 
 }
 bool Earley::InGrammar(const std::string &word) {
   return false;
 }
 
-bool Earley::Situation::operator<(const Earley::Situation &another) const {
+bool operator<(const Earley::Situation &a, const Earley::Situation &b) {
   return false;
+}
+void Earley::Predict(size_t number) {
+
+}
+void Earley::Scan(size_t number, char symbol) {
+
+}
+void Earley::Complete(size_t number) {
+
 }
--- Earley.h
+++ Earley.h
@@ -7,11 +7,14 @@ class Earley {
   explicit Earley(ContextFreeGrammar);
   bool InGrammar(const std::string &word);
  private:
-
+  void Predict(size_t number);
+  void Scan(size_t number, char symbol);
+  void Complete(size_t number);
   class Situation {
    public:
     Situation(ContextFreeGrammar::Rule rule, size_t point, size_t i) : rule(std::move(rule)), point(point), i(i) {};
-    bool operator<(const Situation &another) const;
+
+    friend bool operator<(const Situation &a, const Situation &b);
 
     ContextFreeGrammar::Rule rule;
 
