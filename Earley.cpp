9da8e6afab9a84057c992154a3762c4ba5275465
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2020-12-28 21:51:31 +0300

    a

--- Earley.cpp
+++ Earley.cpp
@@ -3,19 +3,24 @@
 Earley::Earley(ContextFreeGrammar grammar) : grammar_(grammar) {
 
 }
+
 bool Earley::InGrammar(const std::string &word) {
   return false;
 }
 
 bool operator<(const Earley::Situation &a, const Earley::Situation &b) {
-  return false;
+  return a.rule < b.rule || a.rule == b.rule && a.point < b.point
+      || a.rule == b.rule && a.point == b.point && a.i < b.i;
 }
+
 void Earley::Predict(size_t number) {
 
 }
+
 void Earley::Scan(size_t number, char symbol) {
 
 }
+
 void Earley::Complete(size_t number) {
 
 }
