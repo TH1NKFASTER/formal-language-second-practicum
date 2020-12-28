69c2c808c115a4cf6620b44f9a6f791e7629d548
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2020-12-28 21:18:00 +0300

    a

--- Earley.h
+++ Earley.h
@@ -6,10 +6,6 @@ class Earley {
  public:
   explicit Earley(ContextFreeGrammar grammar);
   bool InGrammar(const std::string &word);
- private:
-  void Predict(size_t number);
-  void Scan(size_t number, char symbol);
-  void Complete(size_t number);
   class Situation {
    public:
     Situation(ContextFreeGrammar::Rule rule, size_t point, size_t i) : rule(std::move(rule)), point(point), i(i) {};
@@ -22,5 +18,9 @@ class Earley {
 
     size_t point;
   };
+ private:
+  void Predict(size_t number);
+  void Scan(size_t number, char symbol);
+  void Complete(size_t number);
   ContextFreeGrammar grammar_;
 };
