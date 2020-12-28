cfa372f3afe229d2d2f0e475780571aa11a94777
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2020-12-28 22:06:37 +0300

    a

--- ContextFreeGrammar.h
+++ ContextFreeGrammar.h
@@ -3,18 +3,29 @@
 #include <set>
 class ContextFreeGrammar {
  private:
-  bool CheckRule(const std::string& rule);
+  bool CheckRule(const std::string &rule);
   std::set<char> non_terminals_;
   std::set<char> terminals_;
  public:
   class ERROR;
 
-  class Rule{
+  class Rule {
    public:
     Rule() = default;
+
     Rule(char non_terminal, std::string expression) : non_terminal(non_terminal), expression(std::move(expression)) {}
+
     char non_terminal{};
+
     std::string expression{};
+
+    friend bool operator<(const Rule &a, const Rule &b) {
+      return a.non_terminal < b.non_terminal || a.non_terminal == b.non_terminal && a.expression < b.expression;
+    }
+
+    friend bool operator==(const Rule &a, const Rule &b) {
+      return a.non_terminal == b.non_terminal && a.expression == b.expression;
+    }
   };
 
   explicit ContextFreeGrammar(const std::vector<std::string> &vector_rules);
